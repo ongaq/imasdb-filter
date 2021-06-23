dde0695fbc49df055ac68f8a5aaf14de77ca55f2
ongaq <bascinet@gmail.com>
2021-06-24 00:19:57 +0900

    v1.0.2

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 'use strict';
 
 (async() => {
-  const body = document.querySelector('body > .maruamyu-body');
-  const programList = document.querySelectorAll('.list.imas-ipg-onAirList.ipgOnAirList tr');
-	const getVoiceActorsFromStorage = async () => 
-		new Promise((resolve) => 
-			chrome.storage.local.get('voiceActors', (res) => 
-				resolve(res)));
-	const { voiceActors } = await getVoiceActorsFromStorage();
+  if (location.pathname !== '/bangumi/') {
+    return;
+  }
 
-	if (!voiceActors || !voiceActors.length) {
-    body.style.visibility = 'visible';
+  const body = document.querySelector('body > .maruamyu-body');
+  body.style.visibility = 'hidden';
+  const programList = document.querySelectorAll('.list.imas-ipg-onAirList.ipgOnAirList tr');
+  const getVoiceActorsFromStorage = async () => 
+    new Promise((resolve) => 
+      chrome.storage.local.get('voiceActors', (res) => 
+        resolve(res)));
+  const { voiceActors } = await getVoiceActorsFromStorage();
+
+  if (!voiceActors || !voiceActors.length) {
+    body.style.removeProperty('visibility');
     return;
   }
 
@@ -27,5 +32,5 @@
       }
     }
   });
-  body.style.visibility = 'visible';
+  body.style.removeProperty('visibility');
 })();
