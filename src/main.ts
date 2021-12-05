28b030ed9a8fbb2b16dc66032dc259336a661468
Yuki Hirata <bascinet@gmail.com>
2021-12-05 16:42:43 +0900

    ts修正

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 'use strict';
 
+type VoiceActors = {
+  voiceActors?: string[]
+};
+
 (async() => {
   const isProgram = /^\/bangumi\/(scheduled)?$/.test(location.pathname);
-  const body = document.querySelector('body > .container-fluid') || document.querySelector('body > .maruamyu-body');
+  const body = document.querySelector<HTMLDivElement>('body > .container-fluid') || 
+    document.querySelector<HTMLDivElement>('body > .maruamyu-body');
 
   if (!isProgram || body === null) return;
 
   body.style.visibility = 'hidden';
-  const programList = document.querySelectorAll('.timetable.ipgOnAirList tr') || document.querySelectorAll('.list.imas-ipg-onAirList.ipgOnAirList tr');
-  const getVoiceActorsFromStorage = async () => 
+  const programList = document.querySelectorAll<HTMLTableRowElement>('.timetable.ipgOnAirList tr') || 
+    document.querySelectorAll<HTMLTableRowElement>('.list.imas-ipg-onAirList.ipgOnAirList tr');
+  const getVoiceActorsFromStorage = async(): Promise<VoiceActors> => 
     new Promise((resolve) => 
-      chrome.storage.local.get('voiceActors', (res) => 
-        resolve(res)));
+      chrome.storage.local.get('voiceActors', (res) => resolve(res)));
   const { voiceActors } = await getVoiceActorsFromStorage();
 
   if (!voiceActors || !voiceActors.length) {
@@ -24,9 +29,7 @@
     const text = [...tr.querySelectorAll('td')]
       .reduce((temp, td) => temp += td.innerText.replace(/[\s\n]/g, ''), '');
 
-    for (let i=0,len=voiceActors.length; i < len; i++) {
-      const name = voiceActors[i];
-      
+    for (const name of voiceActors) {
       if (text.indexOf(name) !== -1) {
         tr.style.display = 'table-row';
         break;
